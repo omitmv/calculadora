b3aead31b95cce686c2307137e5e64caa9a0bb58
Rafael Valverde <omit.mv@gmail.com>
2020-06-12 17:07:15 -0300

    First commit.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculadora from './main/Calculadora'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+    <h1>Calculadora</h1>
+    <Calculadora />
+  </>
+  , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
